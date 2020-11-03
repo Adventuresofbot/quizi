1f42bf4fa14d7d7bc7f68ee9846e0a7e4a9cc204
siddiqmo10 <48358947+siddiqmo10@users.noreply.github.com>
2020-11-03 09:47:27 -0500

    Update script.js
    forgot to retrieve end button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
+const endButton = document.getElementById('end-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
